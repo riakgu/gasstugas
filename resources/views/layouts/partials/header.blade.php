d7beacb2e4ca9651fe73365bc356138cb7178d1f
riakgu <rizkyakbargusnaen@gmail.com>
2024-03-30 10:48:26 +0700

    redesign header

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -36,20 +36,18 @@
                     <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
                         <div class="user-menu d-flex">
                             <div class="user-name text-end me-3">
-                                <h6 class="mb-0 text-gray-600">John Ducky</h6>
+                                <h6 class="mb-0 text-gray-600">{{ auth()->user()->name }}</h6>
                                 <p class="mb-0 text-sm text-gray-600">Administrator</p>
                             </div>
                             <div class="user-img d-flex align-items-center">
-                                <div class="avatar avatar-md">
-                                    <img src="./assets/compiled/jpg/1.jpg">
+                                <div class="avatar bg-secondary avatar-md">
+                                    <span class="avatar-content">{{ substr(auth()->user()->name, 0, 1) }}</span>
+                                    <span class="avatar-status bg-success"></span>
                                 </div>
                             </div>
                         </div>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton" style="min-width: 11rem;">
-                        <li>
-                            <h6 class="dropdown-header">Hello, John!</h6>
-                        </li>
                         <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-person me-2"></i> My
                                 Profile</a></li>
                         <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-gear me-2"></i>
