4a5c7e050b4855454e7c232b633c0537beb50df1
riakgu <rizkyakbargusnaen@gmail.com>
2024-04-05 09:05:37 +0700

    update script path

--- resources/views/chatbot/index.blade.php
+++ resources/views/chatbot/index.blade.php
@@ -75,5 +75,5 @@
 
 @section('script')
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="/../assets/js/chatbot.js"></script>
+    <script src="/../assets/js/pages/chatbot.js"></script>
 @endsection
