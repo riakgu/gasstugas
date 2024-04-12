5936c0b6ff9ed8748ca83a197c9bfda2e9d0d853
riakgu <rizkyakbargusnaen@gmail.com>
2024-04-13 06:13:16 +0700

    separate routes based on middleware

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,40 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChatbotController;
+use App\Http\Controllers\SettingsController;
 
 Route::get('/', function () {
     return redirect('/login');
 });
 
-Route::controller(\App\Http\Controllers\HomeController::class)->group(function () {
-    Route::get('/home', 'index')->middleware('auth');
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    Route::resource('tasks', TaskController::class)->except(['show']);
+
+    Route::controller(ChatbotController::class)->group(function () {
+        Route::get('/chatbot', 'index')->name('chatbot');
+        Route::post('/chatbot', 'chatbot');
+    });
+
+    Route::controller(SettingsController::class)->group(function () {
+        Route::get('/settings', 'index')->name('settings');
+        Route::put('/settings/profile', 'updateProfile');
+        Route::put('/settings/password', 'updatePassword');
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
-Route::controller(\App\Http\Controllers\AuthController::class)->group(function () {
-    Route::get('/login', 'login')->name('login')->middleware('guest');
-    Route::post('/login', 'doLogin')->middleware('guest');
-    Route::get('/register', 'register')->name('register')->middleware('guest');
-    Route::post('/register', 'doRegister')->middleware('guest');
-    Route::post('/logout', 'doLogout')->middleware('auth')->name('logout');
-});
-
-Route::controller(\App\Http\Controllers\ChatbotController::class)->group(function () {
-    Route::get('/chatbot', 'index')->middleware('auth');
-    Route::post('/chatbot', 'chatbot')->middleware('auth');
-});
-
-Route::resource('tasks', \App\Http\Controllers\TaskController::class)->middleware('auth');
-
-Route::controller(\App\Http\Controllers\SettingsController::class)->group(function () {
-    Route::get('/settings', 'index')->middleware('auth');
-    Route::put('/settings/profile', 'profile')->middleware('auth');
-    Route::put('/settings/password', 'password')->middleware('auth');
+Route::middleware('guest')->group(function () {
+    Route::controller(AuthController::class)->group(function () {
+        Route::get('/login', 'showLoginForm')->name('login');
+        Route::post('/login', 'login');
+        Route::get('/register', 'showRegistrationForm')->name('register');
+        Route::post('/register', 'register');
+    });
 });
