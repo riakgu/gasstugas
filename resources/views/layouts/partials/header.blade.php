0ae5e6d87dfc4e8fbc488c598401cd3b1d6f7555
riakgu <rizkyakbargusnaen@gmail.com>
2024-04-13 06:37:46 +0700

    update header layout

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -37,7 +37,13 @@
                         <div class="user-menu d-flex">
                             <div class="user-name text-end me-3">
                                 <h6 class="mb-0 text-gray-600">{{ auth()->user()->name }}</h6>
-                                <p class="mb-0 text-sm text-gray-600">Administrator</p>
+                                <p class="mb-0 text-sm text-gray-600">
+                                    @if(auth()->user()->hasRole('admin'))
+                                        Administrator
+                                    @elseif(auth()->user()->hasRole('user'))
+                                        User
+                                    @endif
+                                </p>
                             </div>
                             <div class="user-img d-flex align-items-center">
                                 <div class="avatar bg-secondary avatar-md">
@@ -48,8 +54,6 @@
                         </div>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton" style="min-width: 11rem;">
-                        <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-person me-2"></i> My
-                                Profile</a></li>
                         <li><a class="dropdown-item" href="/settings"><i class="icon-mid bi bi-gear me-2"></i>
                                 Settings</a></li>
                         <li>
