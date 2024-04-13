134310e7937bb6b89ec101ef6a7aef3ef46395b7
riakgu <rizkyakbargusnaen@gmail.com>
2024-04-13 07:15:51 +0700

    add relationship one to many between user and task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,34 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Task extends Model
 {
-    use HasFactory;
 
-    // Menetapkan nama tabel yang terkait dengan model ini
-    protected $table = 'tasks';
-
-    // Menetapkan kunci utama untuk model ini
-    protected $primaryKey = 'task_id';
-
-    // Menentukan field yang bisa diisi melalui mass assignment
     protected $fillable = [
         'user_id',
         'task_name',
         'description',
+        'started',
         'deadline',
         'status',
     ];
 
-    /**
-     * Mendefinisikan hubungan 'belongsTo' dengan model User.
-     *
-     * @return BelongsTo Hubungan antara Task dan User
-     */
-    public function users(): BelongsTo
+    public function user(): BelongsTo
     {
-        // Mengembalikan instance hubungan yang menunjukkan bahwa setiap Task milik sebuah User
-        return $this->belongsTo(User::class, 'user_id', 'user_id');
+        return $this->belongsTo(User::class,'user_id');
     }
+
 }
