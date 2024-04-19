d4f8eef2b4426a419187f44af985c94ddd38cf40
riakgu <rizkyakbargusnaen@gmail.com>
2024-04-19 23:01:43 +0700

    refactoring user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
@@ -28,8 +29,11 @@ class UserController extends Controller
 
     public function create()
     {
+        $roles = Role::all();
+
         return view('users.create', [
             'title' => 'Create User',
+            'roles' => $roles
         ]);
     }
 
@@ -40,6 +44,7 @@ class UserController extends Controller
             'email' => ['required', 'email', 'unique:users', 'max:255'],
             'phone' => ['required', 'regex:/(08)[0-9]{9}/', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'role' => ['required', 'in:admin,user'],
         ]);
 
         $user = User::create([
@@ -49,7 +54,7 @@ class UserController extends Controller
             'password' => Hash::make($validated['password']),
         ]);
 
-        $user->assignRole('user');
+        $user->assignRole($validated['role']);
         // $user->createDefaultCategories();
 
         return redirect('/users')->with('success', 'User has been created!');
@@ -62,9 +67,12 @@ class UserController extends Controller
 
     public function edit(User $user)
     {
+        $roles = Role::all();
+
         return view('users.edit', [
             'title' => 'Edit User',
             'user' => $user,
+            'roles' => $roles
         ]);
     }
 
@@ -75,6 +83,7 @@ class UserController extends Controller
             'email' => ['required', 'email', 'unique:users,email,' . $user->id, 'max:255'],
             'phone' => ['required', 'regex:/(08)[0-9]{9}/', 'unique:users,phone,' . $user->id],
             'password' => ['nullable', 'string', 'min:8', 'confirmed'],
+            'role' => ['required', 'in:admin,user'],
         ]);
 
         $user->update([
@@ -82,6 +91,9 @@ class UserController extends Controller
             'email' => $validated['email'],
             'phone' => $validated['phone'],
         ]);
+
+        $user->removeRole($user->roles->first()->name);
+        $user->assignRole($validated['role']);
 
         if ($request->filled('password')) {
             $user->update([
