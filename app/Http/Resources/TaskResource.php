e57da433a8a95824033526b69fd3feb39dbad66f
riakgu <rizkyakbargusnaen@gmail.com>
2024-05-25 06:55:02 +0700

    add user_id to task resource

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -16,6 +16,7 @@ class TaskResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'user_id' => $this->user_id,
             'category_id' => $this->category_id,
             'task_name' => $this->task_name,
             'description' => $this->description,
