84be44e86196e6dc17597b829b5643f7436c7c50
riakgu <rizkyakbargusnaen@gmail.com>
2024-05-25 13:06:57 +0700

    Change rules and add failed validarion for change pass

--- app/Http/Requests/Auth/ChangePasswordRequest.php
+++ app/Http/Requests/Auth/ChangePasswordRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests\Auth;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ChangePasswordRequest extends FormRequest
 {
@@ -14,8 +16,17 @@ class ChangePasswordRequest extends FormRequest
     public function rules()
     {
         return [
+            'current_password' => ['required'],
             'password' => ['required', 'string', 'min:8'],
 
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => 'Validation failed',
+            'errors' => $validator->errors(),
+        ], 400));
     }
 }
